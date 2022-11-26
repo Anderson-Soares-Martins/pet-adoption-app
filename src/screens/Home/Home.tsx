119190ff0098c960b777720d6a80367486b5a298
Anderson Soares Martins <andersoaresmartins@gmail.com>
2022-11-26 14:38:42 -0300

    Criação da segunda tela

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,28 +1,42 @@
-import { Text, View } from 'react-native';
-import api from '../../services/api';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import type { RootStackParamList } from '../../types';
-import axios from 'axios';
+import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import type { RootStackParamList } from "../../types";
+import Select from "./Select";
+import ListAnimal from "./ListAnimal";
 
 export interface HomeProps {
-  navigation: NativeStackNavigationProp<RootStackParamList, 'Home'>;
+  navigation: NativeStackNavigationProp<RootStackParamList, "Home">;
 }
 
-
 export default function Home({ navigation }: HomeProps) {
-  // comando para rodar o servidor json-server db.json --routes routes.json
-  async function getCategories() {
+  const [selectedCategory, setSelectedCategory] = useState<number>(0);
+
+  async function getDescriptionAnimal() {
     try {
-      const response = await axios.get('http://localhost:3000/categories');
+      const response = await api.get("/animal/1");
       console.log(response.data);
     } catch (error) {
       console.log(error);
     }
   }
-  getCategories();
   return (
     <View>
-      <Text>Ola</Text>
+      <View>
+        <Text>HOME</Text>
+        <Text>Escolha uma categoria para visualizar</Text>
+      </View>
+      <Select
+        currentCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <ListAnimal currentCategory={selectedCategory} />
     </View>
   );
 }
+
+//? 1 - Colocar categoryId em animal = Feito
+//? 2 - Componentizar um compoentne para o select e um para mostrar os animais
+//? 3 - exibir os animais
+//? 4 - fazer ele filtrar pela categoria
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: "http://10.0.0.170:8080",
 });
 
 export default api;
